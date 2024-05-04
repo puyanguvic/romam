bd70121ba0abcdc8133fe0b7f04af409a8700c23
puyanguvic <npuyangpu@gmail.com>
2024-05-03 23:58:55 -0700

    update

--- NSDI2025/exp1/code/exp1.sh
+++ NSDI2025/exp1/code/exp1.sh
@@ -47,6 +47,19 @@ TOPO="geant"
 ./ns3 run "scratch/${PROTCOL}-exp1.cc --topo=${TOPO}"
 
 
+# kshortest
+PROTCOL="kshortest"
+echo "${PROTOCOL}"
+cp "${DIR}code/${PROTCOL}.cc" "scratch/${PROTCOL}-exp1.cc"
+TOPO="abilene"
+./ns3 run "scratch/${PROTCOL}-exp1.cc --topo=${TOPO}"
+TOPO="att"
+./ns3 run "scratch/${PROTCOL}-exp1.cc --topo=${TOPO}"
+TOPO="cernet"
+./ns3 run "scratch/${PROTCOL}-exp1.cc --topo=${TOPO}"
+TOPO="geant"
+./ns3 run "scratch/${PROTCOL}-exp1.cc --topo=${TOPO}"
+
 
 # Octopus
 PROTCOL="octopus"
