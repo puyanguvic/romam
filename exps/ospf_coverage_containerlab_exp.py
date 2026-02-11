602aeb68ea3b4540dfe91c3158f00b0d74ed2563
puyanguvic <npuyangpu@gmail.com>
2026-02-10 16:34:36 -0800

    rm unused script.

--- exps/ospf_coverage_containerlab_exp.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Backward-compatible entrypoint for the renamed convergence experiment."""
-
-from ospf_convergence_exp import main
-
-if __name__ == "__main__":
-    main()
